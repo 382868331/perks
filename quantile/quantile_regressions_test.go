7ed8d06c1ff4ece07ec3e6c9f4afd87efd65e99f
382868331 <382868331@users.noreply.github.com>
2026-08-24 00:05:59 +0800

    test: reproduce 边界上下限顺序 in perks

---
+++ quantile/quantile_regressions_test.go
@@ -0,0 +1,22 @@
+package quantile
+
+import (
+	"context"
+	"errors"
+	"reflect"
+	"testing"
+	"unicode/utf8"
+)
+
+var (
+	_ = context.Background
+	_ = errors.Is
+	_ = reflect.DeepEqual
+	_ = utf8.ValidString
+)
+
+func TestQuantileNormalizeBounds(t *testing.T) {
+	if got := QuantileNormalizeBounds(5, 10, 0); got != 5 {
+		t.Fatalf("got %d", got)
+	}
+}
