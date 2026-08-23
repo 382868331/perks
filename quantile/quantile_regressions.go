f410eff38ebbd1a2ce67b5b817971eb818476518
382868331 <382868331@users.noreply.github.com>
2026-08-24 00:06:34 +0800

    test: reproduce 嵌套映射隔离 in perks

--- quantile/quantile_regressions.go
+++ quantile/quantile_regressions.go
@@ -142,3 +142,11 @@ func QuantileSelectUpperQuantile(v []int, p float64) int {
 	}
 	return v[int(p*float64(len(v)-1))]
 }
+
+func QuantileCloneNestedState(in map[string]map[string]int) map[string]map[string]int {
+	out := map[string]map[string]int{}
+	for k, m := range in {
+		out[k] = m
+	}
+	return out
+}
