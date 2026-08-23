54690dae90a5ee52a1b6937cb17d9062c2b5a3c3
382868331 <382868331@users.noreply.github.com>
2026-08-24 02:35:02 +0800

    fix: restore SampleOrdering contract

--- quantile/task_001_test.go
+++ quantile/task_001_test.go
@@ -12,3 +12,11 @@ func TestTaskPerks001SampleOrdering(t *testing.T) {
 		t.Fatalf("first=%v", v[0].Value)
 	}
 }
+
+func TestTaskPerks001SampleOrderingBoundary(t *testing.T) {
+	v := Samples{{Value: 3}, {Value: 1}, {Value: 2}}
+	sort.Sort(v)
+	if v[2].Value != 3 {
+		t.Fatalf("last=%v", v[2].Value)
+	}
+}
