6c35be263b25e29afe2c6dee4deda9dd6fa18447
382868331 <382868331@users.noreply.github.com>
2026-08-24 02:39:14 +0800

    fix: restore ReplacementCount contract

--- topk/task_019_test.go
+++ topk/task_019_test.go
@@ -12,3 +12,11 @@ func TestTaskPerks019ReplacementCount(t *testing.T) {
 		t.Fatalf("count=%d", q[0].Count)
 	}
 }
+
+func TestTaskPerks019ReplacementCountBoundary(t *testing.T) {
+	s := New(1)
+	s.Merge(Samples{&Element{"a", 2}, &Element{"b", 3}, &Element{"c", 4}})
+	if s.Query()[0].Count <= 4 {
+		t.Fatalf("count=%d", s.Query()[0].Count)
+	}
+}
